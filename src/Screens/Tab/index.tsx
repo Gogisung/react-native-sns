17a943c4e2174be6c461fbed3335437321b76da9
Gogisung <gogosing@inetcop.org>
2020-02-25 09:35:04 +0900

    Tab 만들기

---
+++ src/Screens/Tab/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ImageSourcePropType } from 'react-native';
+
+
+interface Props {
+  selected: boolean;
+  label? : string;
+  imageSource?: ImageSourcePropType;
+  onPress?: () => void;
+}
+
+const Tab = () => {
+
+};
+
+export default Tab;
