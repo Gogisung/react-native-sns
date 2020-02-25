cc7f029788e02f4f1508f2f4a29bad3c7c2f084f
Gogisung <ggs0707@naver.com>
2020-02-25 11:45:49 +0900

    Tab 컴포넌트 이동

---
+++ src/Screens/PasswordReset/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { NavigationScreenProp, NavigationState} from 'react-navigation';
+
+import Styled from 'styled-components/native';
+
+import Input from '~/Components/Input';
+import Button from '~/Components/Button';
+import Tab from '~/Components/Tab';
+
+
+const PasswordReset = () => {
+  return (
+
+  );
+};
+
+export default PasswordReset;
