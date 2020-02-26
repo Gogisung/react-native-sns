ded4665f215d5c010c9f98f97aae1acca643dcb0
Gogisung <ggs0707@naver.com>
2020-02-27 08:22:53 +0900

    NotificationList 수정

---
+++ src/Screens/Notification/NotificationList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotificationList = () => {
+  return(
+
+  );
+};
+
+export default NotificationList;
--- src/Screens/Notification/index.tsx
+++ src/Screens/Notification/index.tsx
@@ -25,7 +25,7 @@ interface Props {}
 const Notification = () => {
   const { getMyFeed } = useContext(RandomUserDataContext);
   const [followingList, setFollowingList] = useState<Array<IFeed>>([]);
-  const [myNotifications, setMyNotifications] = useState<Array<IFeed>>();
+  const [myNotifications, setMyNotifications] = useState<Array<IFeed>>([]);
   const [tabIndex, setTabIndex] = useState<number>();
   const width = Dimensions.get('window').width;
   const tabs = ['팔로잉', '내 소식'];
@@ -54,6 +54,34 @@ const Notification = () => {
           />
         })}
       </ProfileTabContainer>
+      <ScrollView
+        ref={refScrollView}
+        horizontal={true}
+        showsHorizontalScrollIndicator={true}
+        pagingEnabled={true}
+        stickyHeaderIndices={[0]}
+        onScroll={(event: NativeSyntheticEvent<NativeScrollEvent>) => {
+          const index = event.nativeEvent.contentOffset.x / width;
+          setTabIndex(index);
+        }}
+        contentOffset={{ x: width, y: 0 }}>
+        <NotificationList
+          id={0}
+          width={width}
+          data={followingList}
+          onEndReached={() => {
+            setFollowingList([...followingList, ...getMyFeed(24)]);
+          }}
+        />
+        <NotificationList
+          id={1}
+          width={width}
+          data={myNotifications}
+          onEndReached={() => {
+            setMyNotifications([...myNotifications, ...getMyFeed(24)]);
+          }}
+        />
+      </ScrollView>
     </TabContainer>
   );
 };
